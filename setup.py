a2dd30577057e9a206e8077d9b9ded0705123d2c
Your Name <you@example.com>
2021-12-27 10:08:15 +0900

    a

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def resizeImages():
         img = Image.open(imName)
         img_w, img_h = img.width * (Screen.w / myWidth), img.height * (Screen.h / myHeight)
         img_resize = img.resize((int(img_w), int(img_h)), Image.LANCZOS)
-        if imName == "images/lv.bmp":
+        if imName[len(imageDic):] == "lv.bmp":
             img.save(resizedDic + imName[len(imageDic):])
             continue
         img_resize.save(resizedDic + imName[len(imageDic):])
